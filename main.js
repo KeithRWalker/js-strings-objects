f154e89473ae8f688dd3c518b1f63b422ba6b728
Keith Walker <Keithrwalker418@gmail.com>
2019-03-04 21:37:55 -0600

    working with objects

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-// function print(message) {
-//     const outputDiv= document.getElementById('printDiv');
-//     outputDiv.innerHTML += message;
-//   };
-
-  function print(id, message, br) {
+////////////////////////strings//////////////////////////////////////
+const print = (id, message, br) => {
     const selectedDiv = document.getElementById(id);
     selectedDiv.innerHTML += message;
     if(br){selectedDiv.innerHTML +='<br>'};
@@ -27,16 +23,83 @@ const iAmThirsty = (num) => {
 };
 
 console.log(iAmThirsty(21));
-console.log(iAmThirsty(18));
-
 iAmThirsty(65);
 
-print('printDiv', iAmThirsty(100));
+////////////////////////objects//////////////////////////////////////
+
+let hitchhikers_guide = {
+    characters: ["Zaphod", "Arthur", "Ford", "Trillian"],
+    catchphrase: "Don't Panic",
+    random_facts: {
+        copies_sold: 14000000,
+        formats: ["radio", "TV", "film", "graphic novel"],
+        ultimate_answer: {
+            meaning_of_life: 42
+        }
+    }
+};
+
+console.log(hitchhikers_guide.random_facts.ultimate_answer.meaning_of_life);
+
+let employee = {
+    name: "Jeff Winger",
+    age: 37,
+    department: "legal",
+    hire_date: "09/22/2010",
+  }
+
+
+const answer1 = console.log(`Our companies lawyer is ${employee.name}!`);   // 1.
+const answer2 = console.log(`He was hired on ${employee.hire_date}!`);      // 2
+const answer3 = employee.vacation_days = 20;                                // 3
+const answer3b = employee['bracket_vacation'] = 20;                          // 3b
+// the " " in eom require you to use bracket notations since you can't do employee."employee_of_the_month" = false
+let eom = "employee_of_the_month";                          // 4a
+employee[eom] = false;                                       // 4b
+
+
+print('objectDiv', employee.eom + `<br><br>`, true);
+
+const employeeInfo = () => {
+    let name = `NAME: ${employee.name} <br>`;
+    let age = `AGE: ${employee.age} <br>`;
+    let dep = `DEP: ${employee.department} <br>`;
+    let hired = `HIRE_DATE: ${employee.department} <br>`;
+    let vac = `VACATION_DAYS: ${employee.vacation_days} <br>`;
+    let eoms = `EMPLOYEE_OF_THE_MONTH: ${employee.employee_of_the_month} <br>`;
+
+    const a1 = `<br>Our companies lawyer is ${employee.name}!<br>`
+    const a2 = `He was hired on ${employee.hire_date}!<br>`
+    const atitle = `<br>ANSWERS: `
+    
+
+    print('objectDiv', name+age+dep+hired+vac+eoms+atitle+a1+a2+vac+eoms, true);
+
+};
+
+employeeInfo();
 
 
 
-    // under21? drink some water
-    // over21, but under 65? have a beer
-    //over 65 take a nap
+// //////////////////////////////////// example object
+// const catName = () => {
+//     return 'Killer';
+// }
+// const bucketOCats= {
+//     cat1:'fluffy',
+//     cat2: 5,
+//     cat3: cancelAnimationFrame,
+//     cat4: {
+//         water: 'nope',
+//         sunglight: 'yep',
+//     }
+// };
+// console.log(bucketOCats.cat2);
+// console.log(bucketOCats.cat4.water);
 
-    //bonus pring to dom
+// ////////////bracket notation
+// console.log(bucketOCats['cat1']);
+
+
+
+
